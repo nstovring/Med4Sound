4f9bfb05a4e970068537f651128be85abb266a63
deadpanda1 <esberntk@gmail.com>
2016-04-13 15:58:55 +0200

    things happened

---
+++ Assets/Scripts/NetworkStuff.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkStuff : MonoBehaviour{
+
+	// Use this for initialization
+	void Start () {
+        NetworkManager manager = GetComponent<NetworkManager>();
+        manager.connectionConfig.IsAcksLong = true;
+        manager.connectionConfig.MaxSentMessageQueueSize = 512;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
