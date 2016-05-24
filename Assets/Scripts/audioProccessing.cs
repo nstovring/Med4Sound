d7361686391ef2186c4d6356f160cb8c40d53327
Yousif <busskiller@gmail.com>
2016-05-24 15:21:27 +0200

    code dn

--- Assets/Scripts/audioProccessing.cs
+++ Assets/Scripts/audioProccessing.cs
@@ -26,74 +26,27 @@ public class audioProccessing : MonoBehaviour
     void Start()
     {
         player = Instantiate(playerPrefab, playerPrefab.transform.position, Quaternion.identity) as GameObject;
-        recording = AudioClip.Create("theRecording", 100000000, 1, 16000, false);
-
-        monkey = new float[1024];
-        recordedSignal = new List<float>();
-        isRecording = true;
     }
 
     void Update()
     {
         playerMovement();
-        floatArray = GameObject.FindGameObjectWithTag("SkeletonCreator");
-        test = GameObject.Find("testSource").GetComponent<AudioSource>().clip;
-
-        //test.GetData(monkey, 0);
-        //recording.SetData(monkey, 0);
-        //print(monkey);
-
-
-
         audioStuff();
-
-        if (Input.GetKey(KeyCode.P))
-        {
-
-            isRecording = false;
-
-            //monkey = floatArray.GetComponent<AudioAnalyzer>().newSignal;
-            //recording.SetData(floatArray.GetComponent<AudioAnalyzer>().newSignal, 0);
-
-            recording.SetData(recordedSignal.ToArray(), 0);
-
-
-
-            soundplayer.GetComponent<AudioSource>().clip = recording;
-
-            soundplayer.GetComponent<AudioSource>().Play();
-        }
-
-        if (isRecording == false) {
-            print("Play");
-        }
-
-
-        if (Input.GetKey(KeyCode.R))
-        {
-            isRecording = true;
-        }
-
-
-        if (isRecording == true)
-        {
-           recordedSignal = floatArray.GetComponent<AudioAnalyzer>().nikolaj;
-           print("Recording");
-        }
-
-
     }
 
-    float currentime;
 
-
-
-
-
-    void audioStuff()
+    void soundModulator()
     {
 
-        //print(floatArray.GetComponent<AudioAnalyzer>().newSignal);
+        Vector3 soundLocation = GameObject.Find("AudioCalculator").GetComponent<AudioCalculator>().TrackedVector3;
+
+
+        mixer.SetFloat("Tempo", Mathf.Abs(soundLocation.x) / 5);
+        mixer.SetFloat("Pitch", Mathf.Abs(soundLocation.z) / 5);
+
+
+
+
 
 
 
@@ -101,12 +54,15 @@ public class audioProccessing : MonoBehaviour
         mixer.SetFloat("Tempo", Mathf.Abs(movement.x) / 5);
         mixer.SetFloat("Pitch", Mathf.Abs(movement.y) / 5);
 
+
+
+
+
         if (movement.x < 0.2f && movement.x > -0.2f)
         {
             mixer.SetFloat("Pitch", 1);
         }
     }
-
 
     void playerMovement()
     {
